fbc152f9be2540d066fd3da5efd845a9de595d63
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-05-19 09:07:41 -0500

    page not

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+  currentPath: string = '';
 
+  ngOnInit(): void {
+    this.currentPath = window.location.pathname;
+  }
 }
